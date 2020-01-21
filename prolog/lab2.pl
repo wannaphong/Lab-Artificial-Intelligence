92287888d7548ba3fe6a32d7d12913558f26b431
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-01-21 10:09:11 +0700

    Update lab2.pl

--- prolog/lab2.pl
+++ prolog/lab2.pl
@@ -2,10 +2,13 @@ woman(mia).
 woman(jody).
 woman(yolanda).
 man(john).
-man(jody).
 playsAirGuitar(jody).
 party. %i กำหนดว่ามีปาร์ตี้
 %i สอบถาม woman(X). ใครเป็นผู้หญิงบ้าง
 %i playsAirGuitar(X). ใครเล่นกีตาร์บ้าง
 % , ในกาสืบค้นคือ และ
 %i ex playsAirGuitar(X),man(X). ใครเล่นกีตาร์และเป็นผู้ชาย
+
+%i สอบถาม
+%i woman(mia),woman(jody). สอบถามทีละหลายอัน true
+%i woman(mia),woman(john). false
