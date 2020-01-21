eeb6816b73d4871ec8a8b654e4d3ff38ea5c4c83
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-01-21 10:02:08 +0700

    Update lab prolog

--- prolog/lab1.pl
+++ prolog/lab1.pl
@@ -10,7 +10,9 @@ father(bob,john).
 mother(mary,john).
 %i กฎเบื้องต้น
 %i x จะเป็น parent ของ Y ก็ต่อเมื่อ X เป็น father ของ Y
+%i  head:-body
 parent(X,Y):-father(X,Y).
 %i x จะเป็น parent ของ Y ก็ต่อเมื่อ X เป็น mother ของ Y
 parent(X,Y):-mother(X,Y).
 %i parent(X,john). ใครเป็นผู้ปกครองของ john
+%i กรณีไม่รู้ว่ามีอะไรบ้าง สามารถค้นทั้งหมดได้ parent(X,Y). X กับ Y เป็นตัวแปร
---
+++ prolog/lab2.pl
@@ -0,0 +1,11 @@
+woman(mia).
+woman(jody).
+woman(yolanda).
+man(john).
+man(jody).
+playsAirGuitar(jody).
+party. %i กำหนดว่ามีปาร์ตี้
+%i สอบถาม woman(X). ใครเป็นผู้หญิงบ้าง
+%i playsAirGuitar(X). ใครเล่นกีตาร์บ้าง
+% , ในกาสืบค้นคือ และ
+%i ex playsAirGuitar(X),man(X). ใครเล่นกีตาร์และเป็นผู้ชาย
