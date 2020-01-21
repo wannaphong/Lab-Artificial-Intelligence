6b2caaa57bbc690b1ec5d88cdfbfe439f1d66cf4
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-01-21 09:50:39 +0700

    Create test1.pl

---
+++ prolog/test1.pl
@@ -0,0 +1,16 @@
+%i สร้างความจริง
+fish(perch).
+fish(fish_doctor).
+color(red).
+color(green).
+color(yellow).
+%i ใช่ไม่ใช่ ใช้ fish(cat). false คือไม่ปรากฎในฐานความรู้
+%! สืบค้นว่ามีอะไรบ้าง fish(X).
+father(bob,john).
+mother(mary,john).
+%i กฎเบื้องต้น
+%i x จะเป็น parent ของ Y ก็ต่อเมื่อ X เป็น father ของ Y
+parent(X,Y):-father(X,Y).
+%i x จะเป็น parent ของ Y ก็ต่อเมื่อ X เป็น mother ของ Y
+parent(X,Y):-mother(X,Y).
+%i parent(X,john). ใครเป็นผู้ปกครองของ john
