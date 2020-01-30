268a9c4863ff8c211599b3de05c41af3657a499b
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-01-30 23:52:18 +0700

    Update lab2.pl

--- prolog/lab2.pl
+++ prolog/lab2.pl
@@ -6,7 +6,7 @@ playsAirGuitar(jody).
 party. %i กำหนดว่ามีปาร์ตี้
 %i สอบถาม woman(X). ใครเป็นผู้หญิงบ้าง
 %i playsAirGuitar(X). ใครเล่นกีตาร์บ้าง
-% , ในกาสืบค้นคือ และ
+% , ในการสืบค้นคือ และ
 %i ex playsAirGuitar(X),man(X). ใครเล่นกีตาร์และเป็นผู้ชาย
 
 %i สอบถาม
